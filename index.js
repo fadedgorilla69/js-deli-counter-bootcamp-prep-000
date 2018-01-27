4a3c5ad1199d54d43b013b09a49209c77dcd674d
Phillip Green <phillycheez11@gmail.com>
2018-01-27 21:18:55 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,37 @@
+var katzDeli = [];
+
+function takeANumber(line, name){
+  
+  line.push(name)
+
+  return `Welcome, ${name}. You are number ${line.length} in line.`
+}
+
+
+
+
+function nowServing(line){
+  if (!line.length){return "There is nobody waiting to be served!"}
+  
+  const numbersAndNames = [];
+  
+  for (let i = 0, l = line.length; i < l; i++) {
+    numbersAndNames.push(`${i + 1}. ${line[i]}`)
+  }
+
+  return `Currently serving ${numbersAndNames.join(', ')}`
+}
+
+
+
+function currentLine(katzDeliLine){
+    if (!line.length) {
+    return "The line is currently empty."
+  }
+
+  const numbersAndNames = []
+
+  for (let i = 0, l = line.length; i < l; i++) {
+    numbersAndNames.push(`${i + 1}. ${line[i]}`)
+  
+}}
